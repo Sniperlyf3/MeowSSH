5c8007ea728568a4adae42ddd33a559779dd27e8
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-14 07:31:54 +0200

    Fix Android Downloads path expression

--- src/MeowSSH.App/Platforms/Android/AndroidLocalFileTransferService.cs
+++ src/MeowSSH.App/Platforms/Android/AndroidLocalFileTransferService.cs
@@ -73,7 +73,7 @@ public sealed class AndroidLocalFileTransferService : ILocalFileTransferService
         values.Put(MediaStore.IMediaColumns.DisplayName, fileName);
         values.Put(MediaStore.IMediaColumns.MimeType, MimeType(fileName));
         values.Put(MediaStore.IMediaColumns.RelativePath,
-            $"{global::Android.OS.Environment.DirectoryDownloads}/MeowSSH");
+            $"{Android.OS.Environment.DirectoryDownloads}/MeowSSH");
         values.Put(MediaStore.IMediaColumns.IsPending, 1);
 
         var uri = resolver.Insert(MediaStore.Downloads.ExternalContentUri, values)
