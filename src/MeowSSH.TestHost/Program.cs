07e5f48fb5d89ea55c3b9539ac7d95bbbd3efb68
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-15 20:02:32 +0200

    Register encrypted backup test host fake

--- src/MeowSSH.TestHost/Program.cs
+++ src/MeowSSH.TestHost/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddScoped<ISessionLogService>(sp => new FileSessionLogService(
     Path.Combine(Path.GetTempPath(), "meowssh-testhost-session-logs", Guid.NewGuid().ToString("N")),
     sp.GetRequiredService<IEntitlementService>()));
 builder.Services.AddScoped<IAdvancedSftpService, AdvancedSftpService>();
+builder.Services.AddScoped<IEncryptedVaultBackupService, FakeEncryptedVaultBackupService>();
 builder.Services.AddScoped<ProxyJumpConnector>();
 builder.Services.AddScoped<IConnectionEngine>(sp => new SessionLoggingConnectionEngine(
     sp.GetRequiredService<ProxyJumpConnector>(),
