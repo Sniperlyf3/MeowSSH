9c8afeff1f851991d6fd1819ef9bbe874ff77be8
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-15 19:23:36 +0200

    Wire command monitoring services

--- src/MeowSSH.App/MauiProgram.cs
+++ src/MeowSSH.App/MauiProgram.cs
@@ -75,6 +75,11 @@ public static class MauiProgram
         builder.Services.AddSingleton<ICommandActionStore>(_ => new FileCommandActionStore(
             Path.Combine(FileSystem.AppDataDirectory, "actions.json")));
         builder.Services.AddSingleton<ICommandActionService, CommandActionService>();
+        builder.Services.AddSingleton<ICommandMonitorStore>(_ => new FileCommandMonitorStore(
+            Path.Combine(FileSystem.AppDataDirectory, "command-monitors.json")));
+        builder.Services.AddSingleton<ICommandMonitorAlertSink, AndroidCommandMonitorAlertSink>();
+        builder.Services.AddSingleton<CommandMonitoringService>();
+        builder.Services.AddSingleton<ICommandMonitoringService>(sp => sp.GetRequiredService<CommandMonitoringService>());
         builder.Services.AddSingleton<AndroidTailcatVpnController>();
         builder.Services.AddSingleton<ITailcatVpnController>(sp => new EntitlementTailcatVpnController(
             sp.GetRequiredService<AndroidTailcatVpnController>(),
