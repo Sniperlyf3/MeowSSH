9b4bef04f77f9804114600193f5ff8e7839c2a78
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-15 07:43:35 +0200

    Register Tailcat identity portability fake

--- src/MeowSSH.TestHost/Program.cs
+++ src/MeowSSH.TestHost/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<IConnectionEngine, ProxyJumpConnector>();
 builder.Services.AddScoped<ICredentialResolver, FakeCredentialResolver>();
 builder.Services.AddScoped<ISerialDeviceService, UnsupportedSerialDeviceService>();
 builder.Services.AddScoped<ITailcatHubService, FakeTailcatHubService>();
+builder.Services.AddScoped<ITailcatIdentityStore, FakeTailcatIdentityStore>();
 builder.Services.AddScoped(_ => new FakeVaultSession(
     VaultState.Locked,
     recoveryCode: MeowSSH.TestHost.TestHostDefaults.RecoveryCode));
