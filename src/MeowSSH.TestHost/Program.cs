d5ab83ade8ef6dfe5ab491613f1781010ea7f7f3
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-15 18:04:35 +0200

    Register workspace service in test host

--- src/MeowSSH.TestHost/Program.cs
+++ src/MeowSSH.TestHost/Program.cs
@@ -23,6 +23,8 @@ builder.Services.AddScoped<ICredentialResolver, FakeCredentialResolver>();
 builder.Services.AddScoped<ISerialDeviceService, UnsupportedSerialDeviceService>();
 builder.Services.AddScoped<ITailcatHubService, FakeTailcatHubService>();
 builder.Services.AddScoped<ITailcatIdentityStore, FakeTailcatIdentityStore>();
+builder.Services.AddScoped<ITailcatWorkspaceStore, MemoryTailcatWorkspaceStore>();
+builder.Services.AddScoped<ITailcatWorkspaceService, TailcatWorkspaceService>();
 builder.Services.AddScoped<IQrScanner, FakeQrScanner>();
 builder.Services.AddScoped<ITailcatVpnController, FakeTailcatVpnController>();
 builder.Services.AddScoped<IEntitlementService, FakeEntitlementService>();
