170d098a11507da2437579750722eb19476d66c6
Stefan van der Merwe <44154511+Sniperlyf3@users.noreply.github.com>
2026-09-18 11:35:31 +0200

    Remove Tailcat-only state from Telnet

--- src/MeowSSH.Core/Ssh/TelnetConnectionEngine.cs
+++ src/MeowSSH.Core/Ssh/TelnetConnectionEngine.cs
@@ -36,13 +36,6 @@ public sealed class TelnetConnectionEngine : IProtocolConnectionEngine
 
         public Guid HostId { get; } = hostId;
         public bool IsConnected => !_disposed && client.Connected;
-
-        public SshPathStatus? PathStatus => null;
-        public event EventHandler<SshPathStatus>? PathChanged
-        {
-            add { }
-            remove { }
-        }
         public event EventHandler<SshConnectionLost>? ConnectionLost;
 
         public Task<ITerminalSession> OpenTerminalAsync(
